3a49304242cb7dcbf0f62ff92cd069e68cfa5b27
Gabriel Nascimento <76093687+gabbrieln@users.noreply.github.com>
2023-02-24 01:25:27 -0300

    f
    f

---
+++ ListView.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes1
+{
+    public partial class ListView : Form
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpar()
+        {
+            tb_id.Clear();
+            tb_produto.Clear();
+            tb_qtde.Clear();
+            tb_preco.Clear();
+            tb_id.Focus();
+        }
+
+        private void ListView_Load(object sender, EventArgs e)
+        {
+        }
+        private void Obter()
+        {
+            tb_id.Text = lv_produtos.SelectedItems[0].SubItems[0].Text;
+            tb_produto.Text = lv_produtos.SelectedItems[0].SubItems[1].Text;
+            tb_qtde.Text = lv_produtos.SelectedItems[0].SubItems[2].Text;
+            tb_preco.Text = lv_produtos.SelectedItems[0].SubItems[3].Text;
+        }
+
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            if(tb_id.Text == "")
+            {
+                MessageBox.Show("ID não pode ser vazio");
+                tb_id.Focus();
+                return;
+            }
+            if (tb_produto.Text == "")
+            {
+                MessageBox.Show("Produto não pode ser vazio");
+                tb_id.Focus();
+                return;
+            }
+
+            string[] pr = new string[4];
+            pr[0] = tb_id.Text;
+            pr[1] = tb_produto.Text;
+            pr[2] = tb_qtde.Text;
+            pr[3] = tb_preco.Text;
+
+            ListViewItem l = new ListViewItem(pr);
+            lv_produtos.Items.Add(l);
+            Limpar();
+        }
+
+        private void btn_remover_Click(object sender, EventArgs e)
+        {
+            lv_produtos.Items.RemoveAt(lv_produtos.SelectedIndices[0]);
+        }
+
+        private void btn_obter_Click(object sender, EventArgs e)
+        {
+            Obter();
+        }
+
+        private void lv_produtos_SelectedIndexChanged(object sender, EventArgs e)
+        {//SelectedIndexChanged toma uma ação sempre que algum elemento é selecionado.
+            if (lv_produtos.SelectedItems.Count > 0)
+            {
+                Obter();
+            }
+        }
+    }
+}
