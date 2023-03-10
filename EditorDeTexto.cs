aa3ae17214cf2a17b2b8a0265fee93c4e7e22ccf
Gabriel Nascimento <76093687+gabbrieln@users.noreply.github.com>
2023-03-10 03:13:58 -0300

    Add files via upload

---
+++ EditorDeTexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Componentes1
+{
+    public partial class EditorDeTexto : Component
+    {
+        public EditorDeTexto()
+        {
+            InitializeComponent();
+        }
+
+        public EditorDeTexto(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
