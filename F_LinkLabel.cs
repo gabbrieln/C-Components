aff371dc5f8621a83a8b2473ea1a53742cd86f0b
Gabriel Nascimento <76093687+gabbrieln@users.noreply.github.com>
2023-03-22 04:24:10 -0300

    Add files via upload

---
+++ F_LinkLabel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes1
+{
+    public partial class F_LinkLabel : Form
+    {
+        public F_LinkLabel()
+        {
+            InitializeComponent();
+            ll_multiplosLinks.Links.Add(0, 6, "www.google.com");
+            ll_multiplosLinks.Links.Add(9, 5, "http://youtube.com/cfbcursos");
+            ll_multiplosLinks.Links.Add(17, 7, "http://www.youtube.com");
+
+            ll_multiplosLinks.Links[2].Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ll_canal_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://youtube.com"+ tb_nome.Text);
+
+            LinkLabel ll = (LinkLabel)sender;
+            ll_canal.LinkVisited = true;
+        }
+
+        private void F_LinkLabel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ll_calculadora_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc.exe");
+        }
+
+        private void ll_multiplosLinks_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+            e.Link.Visited = true;
+        }
+    }
+}
