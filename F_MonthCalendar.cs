3854ac0c7fdf25f9096fc517db9c127c2b581472
Gabriel Nascimento <76093687+gabbrieln@users.noreply.github.com>
2023-03-26 20:10:45 -0300

    Add files via upload

---
+++ F_MonthCalendar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes1
+{
+    public partial class F_MonthCalendar : Form
+    {
+        public F_MonthCalendar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBox2.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBox3.Text = monthCalendar1.TodayDate.ToString();
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBox2.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+            textBox3.Text = monthCalendar1.TodayDate.ToString();
+        }
+    }
+}
