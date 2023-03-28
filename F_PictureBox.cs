9996c91280d031817a8488205ec1f42e42c7cce4
Gabriel Nascimento <76093687+gabbrieln@users.noreply.github.com>
2023-03-28 03:55:55 -0300

    Add files via upload

---
+++ F_PictureBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes1
+{
+    public partial class F_PictureBox : Form
+    {
+        public F_PictureBox()
+        {
+            InitializeComponent();
+        }
+
+        private void F_PictureBox_Load(object sender, EventArgs e)
+        {
+            label1.Text = Properties.Resources.NomeCanal;
+            label2.Text = Properties.Resources.EnderecoCanal;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources._242683;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources._20210818_173836;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.rabisco;
+
+        }
+    }
+}
